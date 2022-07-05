9ff9bf42594946aac178e79c6e031174c8130606
Mikhail <francuz.blaid@gmail.com>
2022-07-05 23:25:28 +0600

    fix_done. not working sing_out in the device

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
 
   root 'homepage#index'
 
-  #get "/homepage", to: "homepage#index"
-
-  #device indexpage
-  #root 'home#index'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
